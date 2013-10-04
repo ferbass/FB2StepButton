0c4ca060c7abb34c0fe7bd5b932d2d17293c76a8
ferbass <ferbass@gmail.com>
2013-10-04 17:38:25 -0300

    improve sample using table view

--- 2StepButtomDemo/ViewController.m
+++ 2StepButtomDemo/ViewController.m
@@ -9,8 +9,14 @@
 #import "ViewController.h"
 #import "FB2StepButton.h"
 
-@interface ViewController () <FB2StepButtonDelegate>
-@property (weak, nonatomic) IBOutlet UIView *barView;
+@interface ViewController () <FB2StepButtonDelegate, UITableViewDataSource,UITableViewDelegate>
+{
+    NSArray *fruits;
+    NSArray *cars;
+    NSMutableArray *dinamicSections;
+}
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -18,22 +24,82 @@
 
 - (void)viewDidLoad
 {
-    FB2StepButton *fbButtonRight = [[FB2StepButton alloc] initWithDelegate:self position:CGPointMake(295, 5)];
-    fbButtonRight.slide = FB2StepButtonSlideLeft;
-    [self.barView addSubview:fbButtonRight];
+    fruits = @[@"Apple",@"Banana", @"Pineapple", @"Orange", @"Lemon", @"Kiwi"];
+    cars = @[@"Ferrari", @"Bugatti", @"Honda", @"Lamborguini"];
     
-    FB2StepButton *fbButtonLeft = [[FB2StepButton alloc] initWithDelegate:self position:CGPointMake(5, 5)];
-    fbButtonLeft.slide = FB2StepButtonSlideRight;
-    [self.barView addSubview:fbButtonLeft];
+    dinamicSections = [NSMutableArray array];
+    
+    [dinamicSections addObject:fruits];
+    [dinamicSections addObject:cars];
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-#pragma mark FB2StepButtonDelegate
-- (void)clickedButtonWithAction:(FB2StepButtonStep)step
+#pragma mark UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    NSLog(@"%d", step);
+    return dinamicSections.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[dinamicSections objectAtIndex:section] count];
+}
+
+
+#pragma mark UITableViewDelegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    cell.textLabel.text = [[dinamicSections objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *viewHeader = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 30)];
+    viewHeader.backgroundColor = [UIColor colorWithRed:0.247 green:0.294 blue:0.241 alpha:1.000];
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(5, 0, 320, 30)];
+    title.text = [NSString stringWithFormat:@"Title Header %d", section];
+    title.textColor = [UIColor whiteColor];
+    title.backgroundColor = [UIColor clearColor];
+    
+    FB2StepButton *buttom = [[FB2StepButton alloc] initWithDelegate:self position:CGPointMake(295, 5)];
+    buttom.slide = FB2StepButtonSlideLeft;
+    buttom.tag = section;
+
+    [viewHeader addSubview:buttom];
+    [viewHeader addSubview:title];
+    
+    return viewHeader;
+}
+
+- (float)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 30;
+}
+
+#pragma mark FB2StepButtonDelegate
+
+- (void)clickedButtonWithAction:(FB2StepButtonStep)step sender:(id)sender
+{
+    FB2StepButton *b = (FB2StepButton*)sender;
+    if (step == FBButtonActionClear) {
+        [self.tableView beginUpdates];
+        [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:b.tag] withRowAnimation:UITableViewRowAnimationFade];
+        [dinamicSections removeObjectAtIndex:b.tag];
+        [self.tableView endUpdates];
+        
+        NSIndexSet *indexes = [NSMutableIndexSet indexSetWithIndexesInRange:NSMakeRange(0, dinamicSections.count)];
+        [self.tableView reloadSections:indexes withRowAnimation:UITableViewRowAnimationNone];
+    }
 }
 
 - (void)didReceiveMemoryWarning
